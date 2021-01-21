8f8b459d2e320d1b9729744e22bfde309637df22
左岸 <2232726197@qq.com>
2021-01-21 16:56:00 +0800

    feat;(增加启动命令，简化打包部署流程)

--- src/views/classic/components/music.vue
+++ src/views/classic/components/music.vue
@@ -23,97 +23,97 @@
 </template>
 
 <script>
-import { classicBeh } from "@utils/mixin";
-export default {
-  name: "misic",
-  mixins: [classicBeh],
-  props: {
-    src: String,
-    title: String,
-  },
-  data() {
-    return {
-      playing: false,
-      pauseSrc: require("./images/player@pause.png"),
-      playSrc: require("./images/player@play.png"),
-    };
-  },
-  computed: {
-    toggleRotation() {
-      return this.playing == true ? "rotation" : "";
+  import { classicBeh } from '@utils/mixin'
+  export default {
+    name: 'misic',
+    mixins: [classicBeh],
+    props: {
+      src: String,
+      title: String,
     },
-  },
-  methods: {
-    onPlay() {
-      // 图片切换
-      this.playing = !this.playing;
-      let player = this.$refs.player;
-      if (this.playing) {
-        player.play();
-      } else {
-        player.pause();
+    data() {
+      return {
+        playing: false,
+        pauseSrc: require('./images/player@pause.png'),
+        playSrc: require('./images/player@play.png'),
       }
     },
-
-    play() {
-      // 监听 video 原生 api 播放
-      this.playing = true;
+    computed: {
+      toggleRotation() {
+        return this.playing == true ? 'rotation' : ''
+      },
     },
+    methods: {
+      onPlay() {
+        // 图片切换
+        this.playing = !this.playing
+        let player = this.$refs.player
+        if (this.playing) {
+          player.play()
+        } else {
+          player.pause()
+        }
+      },
 
-    pause() {
-      // 监听 video 原生 api 暂停
-      this.playing = false;
+      play() {
+        // 监听 video 原生 api 播放
+        this.playing = true
+      },
+
+      pause() {
+        // 监听 video 原生 api 暂停
+        this.playing = false
+      },
     },
-  },
-};
+  }
 </script>
 
 <style lang="stylus" scoped>
-.container
-	display flex
-	flex-direction column;
-	align-items center;
-	.classic-img
-		width: 4.22rem;
-		height: 4.22rem;
-		margin-top: .6rem;
-		border-radius: 50%;
-	.player-img
-		width: 1.2rem;
-		height: 1.2rem;
-		position: relative;
-		bottom: 2.7rem;
-	.rotation
-		transform rotate(360deg)
-		animation rotation 12s linear infinite
-		@keyframes rotation
-			from
-				transform rotate(0deg)
-			to
-				transform rotate(360deg)
-		// -webkit-transform: rotate(360deg);
-		// animation: rotation 12s linear infinite;
-		// -moz-animation: rotation 12s linear infinite;
-		// -webkit-animation: rotation 12s linear infinite;
-		// -o-animation: rotation 12s linear infinite;
-		// @-webkit-keyframes rotation
-		// 	from {
-		// 		-webkit-transform: rotate(0deg);
-		// 	}
+  .container
+    display flex
+    flex-direction column;
+    align-items center;
+    .classic-img
+      width: 4.22rem;
+      height: 4.22rem;
+      margin-top: .6rem;
+      border-radius: 50%;
+    .player-img
+      width: 1.2rem;
+      height: 1.2rem;
+      position: relative;
+      bottom: 2.7rem;
+    .rotation
+      transform rotate(360deg)
+      animation rotation 12s linear infinite
+      @keyframes rotation
+        from
+          transform rotate(0deg)
+        to
+          transform rotate(360deg)
+      // -webkit-transform: rotate(360deg);
+      // animation: rotation 12s linear infinite;
+      // -moz-animation: rotation 12s linear infinite;
+      // -webkit-animation: rotation 12s linear infinite;
+      // -o-animation: rotation 12s linear infinite;
+      // @-webkit-keyframes rotation
+      // 	from {
+      // 		-webkit-transform: rotate(0deg);
+      // 	}
 
-		// 	to {
-		// 		-webkit-transform: rotate(360deg);
-		// 	}
-	.tag
-		width:.44rem;
-		height:1.28rem;
-		position: relative;
-		bottom: 1.6rem;
-		right:3.1rem;
-	.content
-		display: block;
-		/* width:275px; */
-		max-width: 5.5rem;
-		font-size: 0.36rem;
-		margin-top: -0.9rem;
+      // 	to {
+      // 		-webkit-transform: rotate(360deg);
+      // 	}
+    .tag
+      width:.44rem;
+      height:1.28rem;
+      position: relative;
+      bottom: 1.6rem;
+      right:3.1rem;
+    .content
+      display: block;
+      /* width:275px; */
+      max-width: 5.5rem;
+      font-size: 0.36rem;
+      margin-top: -0.9rem;
 </style>
---
+++ start.sh
@@ -0,0 +1,12 @@
+git pull
+
+npm run build
+
+#删除容器
+docker rm -f demo1 &> /dev/null
+
+#启动容器
+docker run -d --restart=on-failure:5\
+    -p 8080:80 \
+    -v $PWD/dist:/usr/share/nginx/html \
+    --name demo1 nginx
